160d3446e607a0ac804180740d787e52e4b11464
EMARTINEZ1993 <eliana.ingeniera@gmail.com>
2025-09-04 12:59:06 -0500

    Fix: Configurar Vite para manejar JSX en archivos .js y corregir importación

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Proyecto_Five_React/',
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: []
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
